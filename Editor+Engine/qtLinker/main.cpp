619d72d307d90c02b29474aa1fc8bebe80f17385
nemerle <nemerle5@gmail.com>
2014-11-19 09:45:53 +0100

    Actually use the qrc file to store program resources :)

--- Editor+Engine/qtLinker/main.cpp
+++ Editor+Engine/qtLinker/main.cpp
@@ -6,33 +6,33 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-	QPixmap pixmap("DOT_Logo.png"); //Insert your splash page image here
-	if (pixmap.isNull())
-	{
-		QMessageBox::warning(0, "Error", "Failed to load Splash Screen image!");
-	}
-	
-	QSplashScreen splash(pixmap, Qt::WindowFlags(Qt::WindowType::SplashScreen));
-	splash.setMaximumSize(QSize(452, 800));
-	splash.show();
-	splash.showMessage("Loading last save and compiling. Please wait...", Qt::AlignRight | Qt::AlignTop, Qt::black);
-	a.processEvents();
+    QPixmap pixmap(":/qtLinker/DOT_Logo.png"); //Insert your splash page image here
+    if (pixmap.isNull())
+    {
+        QMessageBox::warning(0, "Error", "Failed to load Splash Screen image!");
+    }
 
-	QFile File("QTDark.stylesheet");
-	File.open(QFile::ReadOnly);
-	QString StyleSheet = QLatin1String(File.readAll());
+    QSplashScreen splash(pixmap, Qt::WindowFlags(Qt::WindowType::SplashScreen));
+    splash.setMaximumSize(QSize(452, 800));
+    splash.show();
+    splash.showMessage("Loading last save and compiling. Please wait...", Qt::AlignRight | Qt::AlignTop, Qt::black);
+    a.processEvents();
 
-
-	a.setStyleSheet(StyleSheet);
+    QFile File(":/qtLinker/QTDark.stylesheet");
+    File.open(QFile::ReadOnly);
+    QString StyleSheet = QLatin1String(File.readAll());
 
 
+    a.setStyleSheet(StyleSheet);
 
-	Window w;
-	w.adjustSize();
-	XMLHandler handl = XMLHandler("temp.xml", w.interop->ReturnWorld(), w.interop->ReturnGrid()->grid, false);
-	w.show();
-	splash.finish(&w);
+
+
+    Window w;
+    w.adjustSize();
+    XMLHandler handl = XMLHandler("temp.xml", w.interop->ReturnWorld(), w.interop->ReturnGrid()->grid, false);
+    w.show();
+    splash.finish(&w);
     a.exec();
-	handl = XMLHandler("temp.xml", w.interop->ReturnWorld(), w.interop->ReturnGrid()->grid, true);
-	return 0;
+    handl = XMLHandler("temp.xml", w.interop->ReturnWorld(), w.interop->ReturnGrid()->grid, true);
+    return 0;
 }
